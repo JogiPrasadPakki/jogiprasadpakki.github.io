8cecaceeff58494be651c8e6b244c27d1326f8bc
Jogi Prasad Pakki <p.j.prasad02@gmail.com>
2018-12-06 12:52:25 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
 $(document).ready(function () {
-
-    var url = window.location.href;
-    var indices = [];
-    for(var i=0; i<url.length;i++) {
-    if (url[i] === "/") indices.push(i);
-    }
-    url=url.substring(0,(indices[(indices.length)-1])+1);
-    $("#top-nav").load(url+"models/top-nav.html");
-    $("#fotter").load(url+"models/fotter.html")
-
-    $(window).scroll(function(){
-        $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
-        
-    });
+    $("#top-nav").load("https://jogiprasadpakki.github.io/models/top-nav.html");
+    $("#fotter").load("https://jogiprasadpakki.github.io/models/fotter.html")
 });
